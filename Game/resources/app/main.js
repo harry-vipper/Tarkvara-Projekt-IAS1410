bc9f5e2cac0cfc67458a3f55c29a03ea07f65a34
harry-vipper <harry.vipper@gmail.com>
2020-04-29 09:35:05 +0300

    Window size and settings

--- Game/resources/app/main.js
+++ Game/resources/app/main.js
@@ -3,28 +3,51 @@ const url = require("url");
 const path = require("path");
 
 const {app,BrowserWindow} = electron;
-
+const DEBUG=false;
 
 var mainWindow;
-
-app.on("ready",function(){
-    mainWindow = new BrowserWindow({
-        "fullscreen": false,
-        //frame: false,
-        webPreferences: {
-            nodeIntegration: true,
-            //webSecurity: false
-        }
+if(DEBUG){
+    app.on("ready",function(){
+        mainWindow = new BrowserWindow({
+            width: 1240, 
+            height: 1024, 
+            frame: true,
+            webPreferences: {
+                nodeIntegration: true,
+            }
+        });
+        mainWindow.webContents.openDevTools();
+        mainWindow.on('closed', () => {
+            app.exit(0);
+        })
+            
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname,"Game.html"),
+            protocol:"file:",
+            slashes: true
+        }));
     });
-    mainWindow.webContents.openDevTools();
-    mainWindow.on('closed', () => {
-        app.exit(0);
-      })
-    mainWindow.setAutoHideMenuBar(true);
-        
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,"Game.html"),
-        protocol:"file:",
-        slashes: true
-    }));
-});
+}
+else{
+    app.on("ready",function(){
+        mainWindow = new BrowserWindow({
+            //"fullscreen": true,
+            width: 1240, 
+            height: 1024, 
+            frame: false,
+            webPreferences: {
+                nodeIntegration: true,
+            }
+        });
+        mainWindow.on('closed', () => {
+            app.exit(0);
+        })
+        mainWindow.setAutoHideMenuBar(true);
+            
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname,"Game.html"),
+            protocol:"file:",
+            slashes: true
+        }));
+    });
+}
