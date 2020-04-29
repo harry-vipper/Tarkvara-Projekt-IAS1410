84938761484ae530978be4121d4108791e95e01f
harry-vipper <harry.vipper@gmail.com>
2020-04-29 18:23:19 +0300

    Gamefile input filter

--- Game/resources/app/js/util/savefilesystem.js
+++ Game/resources/app/js/util/savefilesystem.js
@@ -35,6 +35,30 @@ file={
 
             loadFile = fs.readFileSync(games);
             this.content=JSON.parse(loadFile);
+            for(let i=0;i<this.content.content.length;i++){
+                this.content.content[i].id=castToType("number",this.content.content[i].id);
+                this.content.content[i].properties.title=castToType("string",this.content.content[i].properties.title);
+                this.content.content[i].properties.description=castToType("string",this.content.content[i].properties.description);
+                this.content.content[i].properties.players.min=castToType("number",this.content.content[i].properties.players.min);
+                this.content.content[i].properties.players.max=castToType("number",this.content.content[i].properties.players.max);
+                this.content.content[i].properties.volume=castToType("number",this.content.content[i].properties.volume);
+                this.content.content[i].properties.condition=castToType("number",this.content.content[i].properties.condition);
+                this.content.content[i].settings.random=castToType("boolean",this.content.content[i].settings.random);
+                this.content.content[i].settings.contentElementDuration=castToType("number",this.content.content[i].settings.contentElementDuration);
+                this.content.content[i].settings.minigames.mg1=castToType("boolean",this.content.content[i].settings.minigames.mg1);
+                this.content.content[i].settings.minigames.mg2=castToType("boolean",this.content.content[i].settings.minigames.mg2);
+                //this.content.content[i].settings.minigames.mg3=castToType("boolean",this.content.content[i].settings.minigames.mg3);
+                for(let j=0;j<this.content.content[i].contentElements.length;j++){
+                    this.content.content[i].contentElements[j].id=castToType("number",this.content.content[i].contentElements[j].id);
+                    this.content.content[i].contentElements[j].type=castToType("contentElementType",this.content.content[i].contentElements[j].type);
+                    this.content.content[i].contentElements[j].str=castToType("string",this.content.content[i].contentElements[j].str);
+                    this.content.content[i].contentElements[j].repeatable=castToType("boolean",this.content.content[i].contentElements[j].repeatable);
+                    this.content.content[i].contentElements[j].likelyRepeats=castToType("number",this.content.content[i].contentElements[j].likelyRepeats);
+
+
+                }
+            }
+            this.content
         },
         path: __dirname
     },
